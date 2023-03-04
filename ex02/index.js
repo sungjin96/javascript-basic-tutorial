6977901249096f3a93f58698bbb391d06375740b
sungjin96 <sungjin5891@gmail.com>
2023-03-04 21:35:26 +0900

    초기화

--- ex02/index.js
+++ ex02/index.js
@@ -1,51 +1,23 @@
 // 1. 산술연산자
-console.log(`더하기: ${10 + 1}`)
-console.log(`빼기: ${10 - 5}`)
-console.log(`곱하기: ${10 * 2}`)
-console.log(`나누기: ${10 / 2}`)
-console.log(`나머지: ${10 % 3}`)
+
 
 // ===========================
 // 2. 대입연산자
-let number = 10;
-console.log(`더하자마자 저장: ${number += 1}`)
-console.log(`one: ${number}`)
-console.log(`빼자마자 저장: ${number -= 1}`)
-console.log(`one: ${number}`)
-console.log(`곱하자마자 저장: ${number *= 2}`)
-console.log(`one: ${number}`)
-console.log(`나머지 저장: ${number /= 2}`)
-console.log(`one: ${number}`)
+
 
 // ===========================
 // 3. 증감연산자
-console.log(`number: ${number}`)
-console.log(`result: ${number++}`)
-console.log(`number: ${number}`)
-console.log(`result: ${++number}`)
-console.log(`number: ${number}`)
+
 
 // ===========================
 // 4. 비교연산자
-console.log(10 === 10)
-console.log(10 !== 10)
-console.log(10 == "10")
-console.log(10 != "10")
-console.log(10 > 10)
-console.log(10 >= 10)
-console.log(10 < 10)
-console.log(10 <= 10)
+
 
 // ==========================
 // 5. 논리 연산자
-console.log(true && true)
-console.log(true && false)
-console.log(true || true)
-console.log(true || false)
-console.log(!(true && true))
+
 
 // ==========================
 // 6. 삼항 연산자
-console.log(true ? "true" : "false")
 
 // 7. 비트 연산자
